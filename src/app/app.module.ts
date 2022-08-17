b3d599deccd00845f3c22f395eb19847e982203f
Liuboff <kirlyubov@gmail.com>
2022-08-17 23:28:56 +0300

    add cart.cervice.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { CartService } from './cart.service';
 
 @NgModule({
   imports: [
@@ -28,6 +29,7 @@ import { CartComponent } from './cart/cart.component';
     ProductDetailsComponent,
     CartComponent,
   ],
+  providers: [CartService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
